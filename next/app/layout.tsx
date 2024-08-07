b702b450961320d2cef4606e7f5d7347126fad15
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-08-07 11:42:33 +0900

    [feat]GoogleAnalyticsとvercel,speedInsightの導入

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './styles/globals.css'
 import './styles/reset.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import GoogleAnalytics from '@/thirdparty/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <GoogleAnalytics />
+        <meta name="theme-color" content="#c9d6df" />
+      </head>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
