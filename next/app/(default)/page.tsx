23d40ffedaee64e0ed18247e8b6cb0c30ecc793e
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-08-07 23:36:35 +0900

    [fix] 不要なインポートを削除

--- next/app/(default)/page.tsx
+++ next/app/(default)/page.tsx
@@ -1,7 +1,6 @@
 import ChatInterface from '@/(default)/chatInterface'
 import Footer from '@/components/layouts/Footer'
 import Header from '@/components/layouts/Header'
-import GenerateButton from '@/components/common/GenerateButton'
 
 export default function Home() {
   return (
