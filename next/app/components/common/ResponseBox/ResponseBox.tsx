bef2d009555799c245f649355d7a7d3a65e7e3b7
akari24320 <s24320@stn.nagaokaut.ac.jp>
2024-08-08 20:51:51 +0900

    [feat]ResponseBoxの開発

--- next/app/components/common/ResponseBox/ResponseBox.tsx
+++ next/app/components/common/ResponseBox/ResponseBox.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import styles from './ResponseBox.module.css'
 
 export default function ResponseBox() {
-  return <></>
+  return (
+    <div className={styles.responseBox}>
+      <p>
+        responseあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+      </p>
+    </div>
+  )
 }
