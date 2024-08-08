f7544ce8e44260b6e0c003828157befe9e6f8123
A-chan <mtureit@gmail.com>
2024-08-08 15:54:18 +0900

    [feat]InputBoxの開発

--- next/app/components/common/InputBox/InputBox.tsx
+++ next/app/components/common/InputBox/InputBox.tsx
@@ -1,5 +1,33 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import s from './InputBox.module.css'
+import GenerateButton from '@/components/common/GenerateButton'
 
 export default function InputBox() {
-  return <></>
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value)
+  }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault() // Enterキーのデフォルトの動作を防ぐ
+      document.getElementById('generateButton')?.click() // GenerateButtonのクリックイベントを出す
+    }
+  }
+
+  return (
+    <div className={s.inputBox}>
+      <textarea
+        className={s.textarea}
+        placeholder="Type your message..."
+        value={message}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
+      <GenerateButton />
+    </div>
+  )
 }
