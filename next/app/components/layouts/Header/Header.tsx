1c6bb69c002621104dea77bc29115ce2ba576128
akari24320 <s24320@stn.nagaokaut.ac.jp>
2024-08-09 14:54:35 +0900

    [feat]Headerの開発

--- next/app/components/layouts/Header/Header.tsx
+++ next/app/components/layouts/Header/Header.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { BiMenuAltLeft } from 'react-icons/bi'
+import styles from './Header.module.css'
 
 export default function Header() {
-  return <></>
+  return (
+    <header className={styles.header}>
+      <BiMenuAltLeft className={styles.icon} size={50} />
+      <p className={styles.text}>GPT-Logprobs</p>
+    </header>
+  )
 }
