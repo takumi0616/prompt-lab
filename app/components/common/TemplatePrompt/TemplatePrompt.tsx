2ed8af5f3082d3a23f6d0688d262899e300dff1e
A-chan <mtureit@gmail.com>
2024-10-01 23:16:29 +0900

    Normalize line endings to LF

--- app/components/common/TemplatePrompt/TemplatePrompt.tsx
+++ app/components/common/TemplatePrompt/TemplatePrompt.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { FaEnvelope, FaPlane } from 'react-icons/fa'
+import { FaEnvelope, FaPlane, FaPenFancy, FaShoppingCart, FaUtensils } from 'react-icons/fa'
+import { IoLogoWechat } from 'react-icons/io5'
+import { SiLibreofficeimpress } from "react-icons/si";
+import { BiDetail } from "react-icons/bi";
+import { MdOutlineRateReview } from "react-icons/md";
 import styles from './TemplatePrompt.module.css'
 
 interface TemplatePromptProps {
@@ -16,11 +20,55 @@ const templates = [
       'あなたはビジネスマンです。クライアントに対して、プロジェクトの進捗報告メールを書いてください。',
   },
   {
+    title: '買い物リストの作成',
+    subTitle: '一週間分の食材リスト',
+    icon: <FaShoppingCart />,
+    prompt:
+      'あなたは家庭で料理をする人です。1週間分の食材の買い物リストを作成してください。',
+  },
+  {
+    title: '簡単な料理レシピ',
+    subTitle: '10分で作れる料理',
+    icon: <FaUtensils />,
+    prompt: '10分以内で作れる簡単な料理のレシピを提案してください。',
+  },
+  {
     title: '旅行プランの提案',
     subTitle: '東京の3泊4日旅行プラン',
     icon: <FaPlane />,
     prompt:
       'あなたは旅行代理店のスタッフです。3泊4日で楽しめる東京の旅行プランを提案してください。',
+  },
+  {
+    title: 'インタビューの質問',
+    subTitle: ' 作家へのインタビューのための質問のリスト',
+    icon: <FaPenFancy />,
+    prompt:
+      'あなたは記者です。作家へのインタビューのための質問のリストを作成してください。',
+  },
+  {
+    title: '賛否両論者',
+    subTitle: ' 特定のトピックの長所と短所を分析',
+    icon: <IoLogoWechat />,
+    prompt: 'リモートワークとオフィスワークの長所と短所を分析してください。',
+  },
+  {
+    title: '資料作成',
+    subTitle: ' 自社の新しいソフトウェアに関する資料作成',
+    icon: <SiLibreofficeimpress />,
+    prompt: 'あなたは大手企業に勤めるソフトウェアの担当者です。新しいソフトウェアに関する資料の骨子を作成してください。',
+  },
+  {
+    title: '会議アジェンダ作成',
+    subTitle: ' 週次チームミーティングのアジェンダ',
+    icon: <BiDetail />,
+    prompt: 'あなたはチームリーダーです。週次チームミーティングのためのアジェンダを作成してください。',
+  },
+  {
+    title: '製品レビュー作成',
+    subTitle: ' 新製品のスマートフォンに関するレビュー',
+    icon: <MdOutlineRateReview />,
+    prompt: 'あなたはテクノロジーレビュアーです。最新のスマートフォンの製品レビューを作成してください。',
   },
 ]
 
